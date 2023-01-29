d2829f99e46f3f08eb9d8d868d9579df7534acd3
JakubZ399 <zurawikjakub@gmail.com>
2023-01-29 22:52:15 +0100

    Add Build Button

--- Assets/Scripts/SpawnBuilding.cs
+++ Assets/Scripts/SpawnBuilding.cs
@@ -8,9 +8,17 @@ public class SpawnBuilding : MonoBehaviour
 {
     [SerializeField] private GameObject buildingToSpawn;
     [SerializeField] private float hammerCooldownTime = 15;
+    public GameObject buttonResource;
+    public GameObject buttonBuild;
 
     private bool isBuild = false;
     private bool isCooldown = false;
+
+    private void Start()
+    {
+        buttonResource.SetActive(false);
+        buttonBuild.SetActive(true);
+    }
 
     //spawn building
     private void OnMouseDown()
@@ -19,6 +27,8 @@ public class SpawnBuilding : MonoBehaviour
         if (Hammer.isHammer == true && isBuild == false && Hammer.isCooldown == false)
         {
             Instantiate(buildingToSpawn, transform.position, Quaternion.identity);
+            buttonResource.SetActive(true);
+            buttonBuild.SetActive(false);
             isBuild = true;
 
             Debug.Log("Build: " + buildingToSpawn);
